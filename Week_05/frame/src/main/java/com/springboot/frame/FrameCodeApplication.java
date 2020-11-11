d720981c8087bacdaf118f3b67f54a7b000c5676
ipman <61867170@qq.com>
2020-11-11 14:45:10 +0800

    add : sprintboot  frame by 2.2.1 version

---
+++ Week_05/frame/src/main/java/com/springboot/frame/FrameCodeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.frame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameCodeApplication.class, args);
+	}
+
+}
